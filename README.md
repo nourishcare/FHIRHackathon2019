c80a66626a2d3d5f305a97b76d7bc8de477b677f
James Thorp <jamesthorp1@gmail.com>
2019-10-15 09:33:18 +0100

    merge

--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 
 ## Stream 1 - Discharge from hospital to care
 
-**Team:** "Emergency Data"
-
 **Stream Participants:**
 
 * Jamie Hibbard - Nourish
-* James Thorpe - Nourish
+* James Thorp - Nourish
--- src/app.js
+++ src/app.js
@@ -2,123 +2,186 @@ function ViewModel() {
     var self = this;
 
     self.nhsNumber = ko.observable("");
+    self.name = ko.observable("");
+
     self.patient = ko.observable(null);
+    self.foundPatients = ko.observable(null);
+    
     self.patientData = ko.observable(null);
-    self.isReviewing = ko.observable(false);
-    self.medicalMarkdown = ko.observable("");
+    self.patientEncounters = ko.observable(null);
+    self.patientQuestionnaireResponse = ko.observable(null);
+    self.patientConditionResponse = ko.observable(null);
 
     self.nhsNumber.subscribe(function() {
-        self.patient(null);
-        self.patientData(null);
+        reset();
         // Fetch patient ID here.
         $.getJSON("https://data.developer.nhs.uk/ccri-fhir/STU3/Patient?identifier=" + self.nhsNumber(), function(data) {
-            patientData = data["entry"][0];
-            if (patientData) {
-                console.log(patientData["resource"]);
-                self.patient(patientData["resource"]);
-            }
+            processPatients(data);
+        });
+    });    
+    
+    self.name.subscribe(function() {
+        reset();
+        // Fetch patients by name here.
+        $.getJSON("https://data.developer.nhs.uk/ccri-fhir/STU3/Patient?name=" + self.name(), function(data) {
+            processPatients(data);
         });
     });
 
-    self.patientId = ko.computed(function() {
-        if (!(patient = self.patient())) return null;
-        console.log("patientId: " + patient["id"]);
-        return patient["id"];
-    });
+    function processPatients(json) {
+        foundPatients = [];
+        for(var entry of json["entry"]) {
+            foundPatients.push(buildPerson(entry["resource"]));
+        } 
+        self.foundPatients(foundPatients);
+        
+        console.log(foundPatients)
+    }
 
     self.Patient = ko.computed(function() {
         if (!(patient = self.patient())) return null;
 
-        console.log(patient["name"]);
         p = {
-            "name": patient["name"][0]["given"][0] + " " + patient["name"][0]["family"],
-            "birthDay": patient["birthDate"],
+            "name": patient["name"],
+            "birthDay": patient["birthDay"],
             "gender": patient["gender"],
-            "gp": patient["generalPractitioner"][0]["display"],
-            "confirmed": ko.observable(false),
+            "gp": patient["gp"]
         };
-
-        p.confirm = function() {
-            console.log("Confirm")
-            this.confirmed(true);
-        }.bind(p);
-
         return p;
+    });
+
+    self.patientId = ko.computed(function() {
+        if (!(patient = self.patient())) return null;
+        console.log(patient["id"]);
+        return patient["id"];
     });
     
     ko.computed(function() {
-        if (!(patient = self.Patient())) return;
+        if (!(patient = self.patient())) return;
         if (!(patientId = self.patientId())) return;
-
-        console.log(patient);
         if (!patient.confirmed()) return;
 
         console.log("Fetch observations");
 
         $.getJSON("https://data.developer.nhs.uk/ccri-fhir/STU3/Patient?_id=" + patientId + "&_revinclude=*", function(data) {
-            console.log(data);
             self.patientData(data);
-        })
+        });
+
+        $.getJSON("https://data.developer.nhs.uk/ccri-fhir/STU3/Encounter?patient=" + patientId, function(data) {
+            self.patientEncounters(data);
+        });
+
+        $.getJSON("https://data.developer.nhs.uk/ccri-fhir/STU3/QuestionnaireResponse?patient=" + patientId, function(data) {
+            self.patientQuestionnaireResponse(data);
+        });
+
+        $.getJSON("https://data.developer.nhs.uk/ccri-fhir/STU3/Condition?patient=" + patientId, function(data) {
+            self.patientConditionResponse(data);
+        });
     })
 
-    self.removedProcedures = ko.observable([]);
-    self.removedAllergies = ko.observable([]);
-    self.removedConditions = ko.observable([]);
-
     self.Encounters = ko.computed(function() {
-        if (!(patientData = self.patientData())) return null;
+        if (!(patientEncounters = self.patientEncounters())) return null;
+        if (!patientEncounters["entry"]) return null;
 
         encounters = [];
-
-        for(var entry of patientData["entry"]) {
+        console.log(patientEncounters["entry"]);
+        for(var entry of patientEncounters["entry"]) {
             resource = entry["resource"];
-            if (resource["resourceType"] == "Encounter") {
-                encounter = {
-                    "name": resource["type"]["0"]["coding"][0]["display"],
-                };
+            encounter = {
+                "name": resource["participant"][0]["individual"]["display"],
+            };
 
-                if (resource["period"]) {
-                    encounter["dateEnd"] = timeago().format(resource["period"]["end"])
-                } else {
-                    encounter["dateEnd"] = "Date unknown"
+            if (resource["period"]) {
+                date = resource["period"]["end"];
+                if (date == null) {
+                    date = resource["period"]["start"];
                 }
-
-                encounters.push(encounter);
-
+                encounter["date"] = timeago().format(date)
+            } else {
+                encounter["date"] = "Date unknown"
             }
+
+            encounters.push(encounter);
+
         }
 
         return encounters;
     });
 
-    self.EncounterIds = ko.computed(function() {
-        if (!(patientData = self.patientData())) return null;
+    self.QuestionnaireResponses = ko.computed(function() {
+        if (!(patientQuestionnaireResponse = self.patientQuestionnaireResponse())) return null;
+        if (!patientQuestionnaireResponse["entry"]) return null;
 
-        ids = [];
+        questionnaireResponses = [];
 
-        for(var entry of patientData["entry"]) {
+        for(var entry of patientQuestionnaireResponse["entry"]) {
             resource = entry["resource"];
-            if (resource["resourceType"] == "Encounter") {
-                ids.push("Encounter/" + resource["id"]);
-            }
+            items = [];
+            resource["item"].forEach(item => {
+                item["item"].forEach(innerItem => {
+                    answer = "";
+                    if (innerItem["answer"]) {
+                        if (innerItem["answer"][0]["valueCoding"]) {
+                            answer = innerItem["answer"][0]["valueCoding"]["display"];
+                        } else {
+                            answer = innerItem["answer"][0]["valueString"];
+                        }
+                    };
+                    qa = {
+                        "question": innerItem["text"],
+                        "answer": answer          
+                    };
+                    
+                    items.push(qa);
+                })
+            });
+
+            questionnaireResponses.push({
+                "id": resource["id"],
+                "status": resource["status"],
+                "author": resource["author"]["display"],
+                "lastUpdated": timeago().format(resource["meta"]["lastUpdated"]),
+                "items": items
+            });
         }
 
-        return ids;
+        return questionnaireResponses;
+    });
+    
+    self.Conditions = ko.computed(function() {
+        if (!(patientConditionResponse = self.patientConditionResponse())) return null;
+        if (!patientConditionResponse["entry"]) return null;
+
+        conditons = [];
+
+        for(var entry of patientConditionResponse["entry"]) {
+            resource = entry["resource"];
+            conditons.push({
+                "id": resource["id"],
+                "clinicalStatus": resource["clinicalStatus"],
+                "lastUpdated": timeago().format(resource["meta"]["lastUpdated"]),
+                "condition": resource["code"]["text"]
+            });
+        }
+
+        return conditons;
     });
 
     self.Allergies = ko.computed(function() {
         if (!(patientData = self.patientData())) return null;
+        if (!patientData["entry"]) return null;
 
         allergies = [];
 
         for(var entry of patientData["entry"]) {
             resource = entry["resource"];
-            if (resource["resourceType"] == "AllergyIntolerance" && !self.removedAllergies().includes(resource["id"])) {
+            if (resource["resourceType"] == "AllergyIntolerance") {
                 allergies.push({
                     "id": resource["id"],
                     "name": resource["code"]["coding"][0]["display"],
                     "date": timeago().format(resource["assertedDate"]),
-                    "reviewed": ko.observable(true),
+                    "reviewed": ko.observable(true)
                 });
             }
         }
@@ -126,127 +189,38 @@ function ViewModel() {
         return allergies;
     });
 
-    self.Conditions = ko.computed(function() {
-        if (!(patientData = self.patientData())) return null;
-
-        conditions = [];
-
-        for(var entry of patientData["entry"]) {
-            resource = entry["resource"];
-            if (resource["resourceType"] == "Condition" && !self.removedConditions().includes(resource["id"])) {
-                c = {
-                    "id": resource["id"],
-                    "name": resource["code"]["coding"][0]["display"],
-                    "reviewed": ko.observable(true),
-                    "encounterId": null,
-                }
-                if (resource["assertedDate"]) {
-                    c["date"] = timeago().format(resource["assertedDate"])
-                } else {
-                    c["date"] = "Date unknown"
-                }
-                if (resource["context"]) {
-                    c["encounterId"] = self.EncounterIds().indexOf(resource["context"]["reference"]) + 1
-                }
-                conditions.push(c);
-            }
+    function buildPerson(person) {
+        gp = "Not Known";
+        if (person["generalPractitioner"]) {
+            gp = person["generalPractitioner"][0]["display"];
         }
 
-        return conditions;
-    });
+        p = {
+            "id": person["id"],
+            "name": person["name"][0]["given"][0] + " " + person["name"][0]["family"],
+            "birthDay": person["birthDate"],
+            "gender": person["gender"],
+            "gp": gp,
+            "confirmed": ko.observable(false),
+        };
+    
+        p.confirm = function() {
+            self.foundPatients(null);
+            this.confirmed(true);
+            self.patient(this);
+            console.log("Confirm");
+        }.bind(p);
+    
+        return p;
+    }  
 
-    self.Procedures = ko.computed(function() {
-        if (!(patientData = self.patientData())) return null;
-
-        procs = [];
-
-        for(var entry of patientData["entry"]) {
-            resource = entry["resource"];
-            if (resource["resourceType"] == "Procedure" && !self.removedProcedures().includes(resource["id"])) {
-                procs.push({
-                    "id": resource["id"],
-                    "name": resource["code"]["coding"][0]["display"],
-                    "reviewed": ko.observable(true),
-                });
-            }
-        }
-
-        return procs;
-    });
-
-    self.startReview = function() {
-        self.isReviewing(true);
-    };
-
-    function findElementsToRemove(elements) {
-        ids = [];
-        for(var element of elements) {
-            if (!element.reviewed()) {
-                ids.push(element["id"]);
-            }
-        }
-        return ids;
+    function reset() {
+        self.patient(null);
+        self.patientData(null);
+        self.patientEncounters(null);
+        self.patientQuestionnaireResponse(null);
+        self.patientConditionResponse(null);
     }
-
-    self.finishReview = function() {
-        self.removedProcedures(findElementsToRemove(self.Procedures()));
-        self.removedAllergies(findElementsToRemove(self.Allergies()));
-        self.removedConditions(findElementsToRemove(self.Conditions()));
-
-        self.isReviewing(false);
-    };
-
-    self.medicalMarkdown = ko.observable("");
-
-    self.ParsedMedical = ko.computed(function() {
-        linesRaw = self.medicalMarkdown();
-        lines = linesRaw.split(/\r\n|\n|\r/);
-        parsed = [];
-        for(line of lines) {
-            if (g = line.match("PR/ ([0-9]*) (reg)")) {
-                parsed.push({
-                    "title": "Pulse rate",
-                    "value": "Rate " + g[1] + " ," + " regular"
-                })
-            } else if (g = line.match("AVPU/ ([AVPU]+)")) {
-                value = ""
-                switch (g[1]) {
-                    case "A":
-                        value = "Alert";
-                        break;
-                    case "V":
-                        value = "Voice";
-                        break;
-                    case "P":
-                        value = "Pain";
-                        break;
-                    case "U":
-                        value = "Unresponsive";
-                }
-                parsed.push({
-                    "title": "Alertness",
-                    "value": value,
-                })
-            } else if (g = line.match("BP/ ([0-9]{2,3})[ /]{1}([0-9]{2,3})")) {
-                parsed.push({
-                    "title": "Blood Pressure",
-                    "value": g[1] + " / " + g[2],
-                })
-            } else if (g = line.match("T/ ([0-9]{1,2})")) {
-                parsed.push({
-                    "title": "Temperature",
-                    "value": g[1] + "C"
-                })
-            } else {
-                parsed.push({
-                    "title": "",
-                    "value": line
-                })
-            }
-        }
-        console.log(parsed);
-        return parsed;
-    });
 }
 
 var vm = new ViewModel();
